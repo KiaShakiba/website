1078e48bf91824ece14bf2db82a104371d610bb3
Kia Shakiba <kiashakiba@protonmail.com>
2022-09-15 18:50:36 -0400

    Add loading screen

---
+++ components/loading.tsx
@@ -0,0 +1,9 @@
+import styles from '../styles/loading.module.scss';
+
+export default function Loading() {
+	return (
+		<div className={styles.container}>
+			<h2>KIA SHAKIBA</h2>
+		</div>
+	);
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,21 @@
 import type { NextPage } from 'next';
+import { useState } from 'react';
 import Head from 'next/head';
 import Image from 'next/image';
+import Loading from '../components/loading';
 import styles from '../styles/home.module.scss';
 
 export default function Home() {
+	const [loading, setLoading] = useState<boolean>(true);
+
 	return (
 		<>
 			<Head>
 				<title>Kia Shakiba</title>
 				<meta name='description' content='' />
 			</Head>
+
+			{loading && <Loading />}
 
 			<main className={styles.main}></main>
 
