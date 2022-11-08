65a2148bbcf054ea2696d751fdcc4d6459425118
Kia Shakiba <kiashakiba@protonmail.com>
2022-11-08 18:23:05 -0500

    Add initial resize hook

---
+++ lib/hooks/responsive.ts
@@ -0,0 +1,6 @@
+import { useState, useEffect } from 'react';
+
+import elementResizeDetecorMaker from 'element-resize-detector';
+
+export function useResponsive() {
+}
--- pages/cv.tsx
+++ pages/cv.tsx
@@ -6,14 +6,21 @@ import Layout from '../components/layout';
 import Loading from '../components/loading';
 import Header from '../components/header';
 
-import styles from '../styles/home.module.scss';
+import styles from '../styles/cv.module.scss';
 
 export default function Cv() {
+
+	function onDocumentLoad({ numPages }) {
+		console.log(numPages);
+	}
+
 	return (
 		<Layout>
 			<Header theme='light' />
 
 			<main className={styles.main}>
+				<Document file='/cv.pdf'>
+				</Document>
 			</main>
 
 			<footer className={styles.footer}></footer>
