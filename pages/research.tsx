e17293fc22cfd667c3fa69c70eb7ef1166448d52
Kia Shakiba <kiashakiba@protonmail.com>
2023-09-23 17:39:48 -0400

    Update content

--- pages/research.tsx
+++ pages/research.tsx
@@ -1,5 +1,7 @@
+import Link from 'next/link';
 import Layout from '../components/layout';
 import Content from '../components/content';
+import { Pages } from '../components/header';
 
 import styles from '../styles/research.module.scss';
 
@@ -9,7 +11,7 @@ export default function Research() {
 			<Content className={styles.container}>
 				<h1>My research</h1>
 
-				<p>My current area of research is the optimization of in-memory caches. Particulary, I am focused on managing the allocation of cache instance in multi-tenant caching servers. Some of my topics of research are:</p>
+				<p>My current area of research is the optimization of in-memory caches. Particulary, I am focused on managing the allocation of cache instances in multi-tenant caching servers. Some of my topics of research are:</p>
 
 				<ul>
 					<li>Eviction policy-specific miss ratio curve generation</li>
@@ -17,7 +19,7 @@ export default function Research() {
 					<li>Resource allocation on multi-tenant caching servers</li>
 				</ul>
 
-				<p>If you have any questions, please feel free to contact me!</p>
+				<p>If you have any questions, please feel free to <Link href={Pages.CONTACT}>contact me</Link>!</p>
 			</Content>
 		</Layout>
 	);
