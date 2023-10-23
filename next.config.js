ef8dd17d28609c6eccc6fd82db0f6ceeff11c412
Kia Shakiba <kiashakiba@protonmail.com>
2023-10-23 19:41:12 -0400

    Remove deploy options from config

--- next.config.js
+++ next.config.js
@@ -3,12 +3,6 @@ const nextConfig = {
 	reactStrictMode: true,
 	swcMinify: true,
 
-	basePath: '/website',
-
-	images: {
-		unoptimized: true
-	},
-
 	webpack(config) {
 		config.module.rules.push({
 			test: /\.svg$/i,
