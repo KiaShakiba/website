34634e0f45e9b7403aa20e421b27919cbdc0937a
Kia Shakiba <kiashakiba@protonmail.com>
2023-11-12 16:35:24 -0500

    Fix incorrect location

--- pages/photos.tsx
+++ pages/photos.tsx
@@ -1,6 +1,6 @@
 import Layout from '../components/layout';
 import Photos from '../components/photos-page/photos';
-import photos from '../components/photos-page/photos.json';
+import photos from '../lib/photos.json';
 
 export default function PhotosPage() {
 	return (
