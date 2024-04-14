39e0f68bae07bbdd58888936b4e035a56e482456
Kia Shakiba <kiashakiba@protonmail.com>
2024-04-13 23:07:27 -0400

    Add publications section

---
+++ components/research-page/publication.tsx
@@ -0,0 +1,112 @@
+import styles from '../../styles/research-page/publication.module.scss';
+
+export type Props = {
+	title: string;
+	authors: Array<Author>;
+
+	conference: Conference;
+	pages: Pages;
+
+	links: Links;
+};
+
+export default function Publication(props: Props) {
+	const { conference, pages, links } = props;
+
+	return (
+		<div className={styles.container}>
+			<a href={links.page} className={styles.title}>{props.title}</a>
+
+			<div className={styles.authors}>
+				{(() => {
+					return props.authors.map((author, i) => {
+						let elements =[getAuthor(author, i)];
+
+						if (i === props.authors.length - 2) {
+							elements.push(
+								<span key={`sep-${i}`}>, and </span>
+							);
+						} else if (i < props.authors.length - 2) {
+							elements.push(
+								<span key={`sep-${i}`}>, </span>
+							);
+						}
+
+						return elements;
+					});
+				})()}
+			</div>
+
+			<div className={styles.info}>
+				<a href={conference.link} target='_blank'>{conference.name}</a>
+				<span>{`, ${conference.month}, ${conference.year}, pp. ${pages.start}-${pages.end}`}</span>
+			</div>
+
+			<div className={styles.links}>
+				{(() => {
+					let elements = [
+						getLink('PAGE', links.page)
+					];
+
+					if (links.pdf) {
+						elements.push(getLink('PDF', links.pdf));
+					}
+
+					if (links.slides) {
+						elements.push(getLink('SLIDES', links.slides));
+					}
+
+					if (links.video) {
+						elements.push(getLink('VIDEO', links.video));
+					}
+
+					return elements;
+				})()}
+			</div>
+		</div>
+	);
+}
+
+function getAuthor(author: Author, key: number): React.ReactNode {
+	if (author.link) {
+		return (
+			<a href={author.link} key={`name-${key}`}>{author.name}</a>
+		);
+	} else {
+		return (
+			<span key={`name-${key}`}>{author.name}</span>
+		);
+	}
+}
+
+function getLink(title: string, link: string): React.ReactNode {
+	return [
+		<span key={0}>[</span>,
+		<a href={link} target='_blank' key={1}>{title}</a>,
+		<span className={styles.bracketClosed} key={2}>]</span>
+	];
+}
+
+type Author = {
+	name: string;
+	link?: string;
+};
+
+type Links = {
+	page: string;
+	pdf?: string;
+	slides?: string;
+	video?: string;
+};
+
+type Conference = {
+	name: string;
+	link: string;
+	month: string;
+	year: number;
+};
+
+type Pages = {
+	start: number;
+	end: number;
+};
--- pages/research.tsx
+++ pages/research.tsx
@@ -2,8 +2,39 @@ import Link from 'next/link';
 import Layout from '../components/layout';
 import Content from '../components/content';
 import { Pages } from '../components/header';
+import Publication, { Props as PublicationProps } from '../components/research-page/publication';
 
 import styles from '../styles/research.module.scss';
+
+const publications: Array<PublicationProps> = [
+	{
+		title: 'Kosmo: Efficient Online Miss Ratio Curve Generation for Eviction Policy Evaluation',
+		authors: [
+			{ name: 'Kia Shakiba', link: 'https://kiashakiba.ca' },
+			{ name: 'Sari Sultan' },
+			{ name: 'Michael Stumm', link: 'https://www.michaelstumm.com' }
+		],
+
+		conference: {
+			name: 'Conf. on File and Storage Technologies (FAST\'24)',
+			link: 'https://www.usenix.org/conference/fast24',
+			month: 'February',
+			year: 2024
+		},
+
+		pages: {
+			start: 89,
+			end: 105
+		},
+
+		links: {
+			page: 'https://www.usenix.org/conference/fast24/presentation/shakiba',
+			pdf: 'https://www.usenix.org/system/files/fast24-shakiba.pdf',
+			slides: 'https://www.usenix.org/system/files/fast24_slides-shakiba.pdf',
+			video: 'https://www.youtube.com/watch?v=67QWxt4QZ38'
+		}
+	}
+];
 
 export default function Research() {
 	return (
@@ -21,6 +52,15 @@ export default function Research() {
 				</ul>
 
 				<p>If you have any questions, please feel free to <Link href={Pages.CONTACT}>contact me</Link>!</p>
+
+				<h2>Publications</h2>
+					{(() => {
+						return publications.map((publication, i) => {
+							return (
+								<Publication {...publication} key={i} />
+							);
+						});
+					})()}
 			</Content>
 		</Layout>
 	);
